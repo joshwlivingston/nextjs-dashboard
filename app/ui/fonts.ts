0bb8a07f216eaa6c5d9e7fbbd0c7f9e4a313a04e
Josh Livingston <joshwlivingston@gmail.com>
2024-11-22 12:46:23 -0700

    begin nextjs tutorial

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import '@/app/ui/global.css';
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: '400' });
